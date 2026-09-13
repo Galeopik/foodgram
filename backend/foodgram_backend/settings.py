82440ff19c32ae4bee48bf744362cb698950a7cc
Gleb Belousov <galeopik@gmail.com>
2026-09-13 16:42:03 +0300

    push

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -12,16 +12,12 @@ SECRET_KEY = os.getenv(
     'django-secure-default-key-for-development'
 )
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-# DEBUG = os.getenv('DEBUG', 'True') == 'True'
-
-ALLOWED_HOSTS = []
-
-# ALLOWED_HOSTS = os.getenv(
-#     'ALLOWED_HOSTS',
-#     'localhost,127.0.0.1'
-# ).split(',')
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1'
+).split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
